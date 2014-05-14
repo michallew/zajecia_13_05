ddadd37860120adbb49a1cf69ea57924dbc3ad08
michallew <michal.lewandowski89@gmail.com>
2014-05-14 11:07:22 +0200

    Obie galezie polaczone

--- kalkulator.py
+++ kalkulator.py
@@ -1,20 +1,17 @@
-<<<<<<< HEAD
 def dodawanie(a,b):
    return a+b
+
+def get_info():
+    print('Ten program dodaje dwie funkcje')
+
+    
+
 l1 =int(input('Wprowadz pierwsza liczbe'))
 l2 =int(input('Wprowadz druga liczbe'))
-
-print(dodawanie(l1,l2))    
-=======
-def dodawanie():
-    a= int(input('Wprowadz pierwsza liczbe'))
-    b= int(input('Wprowadz druga liczbe'))
-    print (a+b)
     
-def get_info():
-    print('Ten program dodaje dwie funkcje;)
+
     
 get_info()    
-dodawanie()    
->>>>>>> dodanie_funkcji_get_info
+print(dodawanie(l1, l2))
+
     
